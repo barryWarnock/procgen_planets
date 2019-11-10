20d7a295342d9288e24b1b8c57a5e33e1ce7e777
Barry Warnock <brrwarnock@gmail.com>
2019-11-09 16:24:17 -0800

    Use octaves in terrain gen
    also added uvs to the mesh, whatever the hell that is

--- Assets/PlanetGen.cs
+++ Assets/PlanetGen.cs
@@ -5,62 +5,94 @@ using UnityEngine;
 public class PlanetGen : MonoBehaviour
 {
     public int maxHeight = 5000;
-    public int seaHeight = 3000;
     public int minHeight = 2500;
-    public int numChunks = 10;
-    public float frequency = 1;
-    public float seed = 0;
-    public float power = 1;
+    public int numVertices = 10;
+    public int octaves = 4;
+    public float lacunarity = 2;
+    public float persistance = 0.5f;
+    public float seed;
     public int dayLength = 100;
-    List<Vector3> generateChunks(int numChunks) {
-        Debug.Log("generate chunks");
-        Vector3 center = Vector3.zero;
-        List<Vector3> chunkBoundaries = new List<Vector3>(numChunks+1);
-        chunkBoundaries.Add(center);
-
-        float theta = 365f/numChunks;
-        Debug.Log(theta);
-
-        for (float rot=0; rot < 365; rot += theta) {
-            Debug.Log(rot);
-            int heightRange = maxHeight - minHeight;
-
-            float height = Mathf.Pow(Mathf.PerlinNoise(rot*frequency, seed), power) * heightRange + minHeight;
-            Vector3 boundary = Quaternion.AngleAxis(rot, Vector3.back) * (Vector3.up * height);
-            chunkBoundaries.Add(boundary);
-        }
-
-        Debug.Log("generated chunks");
-        return chunkBoundaries;
-    }
-    Vector3[] fillChunks(List<Vector3> chunkBoundaries) {
-        Debug.Log("fill chunks");
-        return chunkBoundaries.ToArray();
-    }
+    protected float theta;
     int[] generateTriangles(Vector3[] vertices) {
-        Debug.Log("generate triangles");
         int[] triangles = new int[(vertices.Length-1) * 3];
         for (int i = 1; i < vertices.Length; i++) {
             int triangleIndex = (i-1)*3;
-            triangles[triangleIndex] = i;
+            triangles[triangleIndex] = (i!=vertices.Length-1) ? i+1 : 0;
             triangles[triangleIndex+1] = 0;
-            triangles[triangleIndex+2] = (i!=vertices.Length-1) ? i+1 : 1;
+            triangles[triangleIndex+2] = i;
         }
         return triangles;
+    }
+    void generateMesh() {
+        var mesh = new Mesh();
+        Vector3 center = Vector3.zero;
+        List<Vector3> chunkBoundaries = new List<Vector3>(numVertices+1);
+        chunkBoundaries.Add(center);
+
+        theta = 365f/numVertices;
+
+        for (float rot=0; rot < 365; rot += theta) {
+            Vector3 newVertex = Quaternion.AngleAxis(rot, Vector3.back) * (Vector3.up * maxHeight);
+            chunkBoundaries.Add(newVertex);
+        }
+
+        mesh.vertices = chunkBoundaries.ToArray();
+        mesh.triangles = generateTriangles(mesh.vertices);
+        
+        var meshfilter = this.GetComponent<MeshFilter>();
+        meshfilter.mesh = mesh;
+    }
+    float perlinCircle(float index, float frequency) {
+        float angle = ((index-1) * theta) * Mathf.Deg2Rad;
+        float offset = frequency + seed;
+;
+        float x = frequency * Mathf.Cos(angle);
+        float y = frequency * Mathf.Sin(angle);
+        return Mathf.PerlinNoise(x+offset, y+offset);
+    }
+    float perlinOctaves(float index) {
+        float total = 0;
+        float maxValue = 0;
+        for (int octave = 0; octave < octaves; octave++) {
+            float frequency = Mathf.Pow(lacunarity, octave);
+            float amplitude = Mathf.Pow(persistance, octave);
+            maxValue += amplitude;
+
+            total += perlinCircle(index, frequency) * amplitude;
+        }
+        return total/maxValue;
+    }
+    public void setHeights() {
+        seed = Random.Range(0, 1000000);
+        int heightRange = maxHeight - minHeight;
+        var meshfilter = this.GetComponent<MeshFilter>();
+        Vector3[] vertices = meshfilter.mesh.vertices;
+
+        float theta = 365f/numVertices;
+
+        for (int i=1; i < vertices.Length; i++) {
+            Debug.Log(perlinOctaves(i));
+            float height = perlinOctaves(i) * heightRange + minHeight;
+            vertices[i] = vertices[i].normalized * height;
+        }
+
+        meshfilter.mesh.vertices = vertices;
+        Vector2[] uvs = new Vector2[vertices.Length];
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(0.5f +(vertices[i].x)/(2*maxHeight), 
+                             0.5f + (vertices[i].y)/(2*maxHeight));
+        }
+        meshfilter.mesh.uv = uvs;
+        meshfilter.mesh.RecalculateNormals();
     }
     // Start is called before the first frame update
     void Start()
     {
-        var mesh = new Mesh();
-        List<Vector3> chunkBoundaries = generateChunks(numChunks);
-        Vector3[] vectors = fillChunks(chunkBoundaries);
-        mesh.vertices = vectors;
-        mesh.triangles = generateTriangles(vectors);
-
-        var meshfilter = this.GetComponent<MeshFilter>();
-        meshfilter.mesh = mesh;
+        generateMesh();
+        setHeights();
     }
-
     private void FixedUpdate() {
         float rotationPerSec = 365f / dayLength;
         this.transform.rotation = Quaternion.AngleAxis(rotationPerSec * Time.deltaTime, Vector3.back) * this.transform.rotation;
---
+++ Assets/PlanetGenEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PlanetGen))]
+public class PlanetGenEditor : Editor
+{
+public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PlanetGen pgen = (PlanetGen)target;
+        if(GUILayout.Button("Make Planet"))
+        {
+            pgen.setHeights();
+        }
+    }
+}
